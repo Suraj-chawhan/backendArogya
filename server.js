9c3674c9d5b1c4c57c9a39d7b26df5c235c37cb3
Suraj-chawhan <surajchauhan442918@gmail.com>
2025-03-22 07:02:26 +0530

    new

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(MONGO_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
